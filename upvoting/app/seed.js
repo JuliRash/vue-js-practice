e42cb805e71cd688723f66cda729f8ee0c0eb295
JuliRash <idowujulius92@gmail.com>
2019-01-03 02:08:25 +0100

    created files and vue instance

---
+++ upvoting/app/seed.js
@@ -0,0 +1,11 @@
+const submissions = [
+    {
+        id: 1,
+        title: "Yellow Pail",
+        description: "here is my own description",
+        url: "https://vuejs.com",
+        votes: 16,
+        avatar: '../public/images/avatars/daniel.jpg',
+        submissionImage: '../public/images/submissions/image-yellow.png'
+    }
+]
