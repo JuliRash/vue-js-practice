0c9e2aa9a46b6e0e3a0397317ee95fe9c12cbadb
JuliRash <idowujulius92@gmail.com>
2019-01-03 03:05:11 +0100

    Event handling v-on directive

--- upvoting/app/main.js
+++ upvoting/app/main.js
@@ -1,13 +1,21 @@
 new Vue({
     el: "#app",
     data: {
-        submission: submissions
+        submissions: submissions
     },
     computed: {
         sortedSubmissions () {
-            return this.submission.sort((a, b) => {
+            return this.submissions.sort((a, b) => {
                 return b.votes - a.votes
             });
+        }
+    }, 
+    methods: {
+        upvote (submissionId) {
+            const submission = this.submissions.find(
+                submission => submission.id === submissionId
+            );
+            submission.votes++;
         }
     }
 })
