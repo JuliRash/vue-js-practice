5c64ae5474ff758b1596ce93c81af8d370eca205
JuliRash <idowujulius92@gmail.com>
2019-01-03 02:33:51 +0100

    vote sorting added

--- upvoting/app/main.js
+++ upvoting/app/main.js
@@ -1,7 +1,13 @@
- 
 new Vue({
     el: "#app",
     data: {
         submission: submissions
+    },
+    computed: {
+        sortedSubmissions () {
+            return this.submission.sort((a, b) => {
+                return b.votes - a.votes
+            });
+        }
     }
 })
